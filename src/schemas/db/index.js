c819ba22aaed048f9e4c59e84274dcc93e3d4a20
LA Agong <la@buri.io>
2020-03-29 22:31:42 +0800

    Initial models

--- src/schemas/db/index.js
+++ src/schemas/db/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const uri = process.env.DATABASE_CONNECTION || 'mongodb://localhost:27017/traceph';
+const uri =
+  process.env.DATABASE_CONNECTION || 'mongodb://localhost:27017/traceph';
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.createConnection(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-connection.on('connected', () => console.log('Connection to database successful!'));
-connection.on('error', (e) => console.log('Database error:', e.message));
+connection.on('connected', () =>
+  console.log('Connection to database successful!')
+);
+connection.on('error', e => console.log('Database error:', e.message));
 
 const deviceSchema = require('./devices')(connection);
+const nodeContactSchema = require('./node-contacts')(connection);
+const nodeSchema = require('./nodes')(connection);
 
 module.exports = {
-    deviceSchema
+  deviceSchema,
+  nodeContactSchema,
+  nodeSchema
 };
---
+++ src/schemas/db/node-contacts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.exports = connection => {
+  const NodeContactSchema = new Schema(
+    {
+      // Handling of different types of contact
+      type: {
+        type: Schema.Types.String,
+        enum: ['direct-bluethooth', 'direct-network', 'indirect', 'manual'],
+        required: true
+      },
+
+      // Must be client generated since offline storage of data is assumed.
+      timestamp: { type: Schema.Types.Date, required: true },
+
+      // This field refer to the node who sent this record.
+      source_node_id: { type: Schema.Types.String, required: true },
+
+      // As per TOR the pairings must be stored such that node_id_a < node_id_b.
+      // A node_pair with a single data is allowed to handle 'Indirect' contact. See TOR 2c.
+      node_pairs: [{ type: Schema.Types.String, required: true }],
+
+      // Location stored in GeoJSON format
+      location: {
+        type: {
+          type: Schema.Types.String, // Don't do `{ location: { type: String } }`
+          enum: ['Point'], // 'location.type' must be 'Point'
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+      },
+
+      // Not clear on the TOR if this is determined on the client side or server side.
+      // We'll assume that this is determined on the server.
+      ip: { type: Schema.Types.String, required: true }
+    },
+    {
+      timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
+  );
+
+  return connection.model('NodeContact', NodeContactSchema);
+};
