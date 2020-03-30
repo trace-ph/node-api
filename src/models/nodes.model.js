006b1f81c66fef78268a82080bded07a60ba7f3d
LA Agong <la@buri.io>
2020-03-30 11:12:50 +0800

    apply changes from PR #1

---
+++ src/models/nodes.model.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NodeSchema = new Schema(
+  {
+    // As per TOR, this value must be determined based on device_id + person_id.
+    node_id: { type: Schema.Types.String, required: true },
+
+    device_id: { type: Schema.Types.String, required: true },
+
+    person_id: { type: Schema.Types.String, required: true }
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  }
+);
+
+module.exports = mongoose.model('Node', NodeSchema);
