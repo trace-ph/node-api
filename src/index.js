1a8999fb39d6821442865b82f848acfb39a4f579
Patrick Maurice Pingol <pmcpingol@gmail.com>
2020-04-02 20:38:32 +0800

    Removed unsued vars

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 require('dotenv-safe').config();
 require('./configs/database.config').config();
 
-const { API_PORT, API_URL } = process.env;
 const express = require('express');
 const bodyParser = require('body-parser');
 const Routes = require('./routes');
