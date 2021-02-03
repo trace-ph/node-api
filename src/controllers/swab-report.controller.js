cebad0c19e4eccc14607455a6eca41c6f4f9c770
prokorpio <sanchez.christopherjeff@gmail.com>
2021-02-03 14:28:39 +0800

    Simplify controller & service for swab reporting. Still needs linting.

--- src/controllers/swab-report.controller.js
+++ src/controllers/swab-report.controller.js
@@ -1,35 +1,48 @@
 /* eslint-disable camelcase */
 const nodeContactService = require('~services/node-contact.service');
 
-const { getContactsInWindow } = nodeContactService;
+const { getContactsInRange } = nodeContactService;
 const { convertToDuration } = nodeContactService;
 const { rssiCalibration } = nodeContactService;
-const { filter } = nodeContactService;
 
 // SAMPLE INPUT:
-// const node_id = '123';
-// const ref_date = new Date('2021-01-02'); // onset of illness, x
-// const result_date = new Date('2021-01-07'); // x+a
+/*
+const node_id = '0ea94fee138c58c';
+const ref_date = new Date('2021-02-25'); // onset of illness, x
+const result_date = new Date('2021-02-30'); // x+a
+*/
 
 // get close contacts
-async function getCloseContact(node_id, ref_date, result_date) {
-  let window_contacts = await getContactsInWindow(node_id, ref_date, result_date);
-  window_contacts = convertToDuration(window_contacts);
-  window_contacts = rssiCalibration(window_contacts);
+async function getCloseContacts(node_id, ref_date, result_date) {
+  // let direct= await getContactsInRange(node_id,
+  // ref_date, result_date, [-35,-30]);
 
-  const direct = filter(window_contacts, query = '2m<dist<=3m and dur<=15min');
-  const proximal = filter(window_contacts, query = 'dist <=2m');
+  console.time('getContactsInRange');
+  let proximal = await getContactsInRange(
+    node_id, ref_date, result_date, [-50, 1],
+  );
+  console.timeEnd('getContactsInRange');
 
-  return { direct, proximal };
+  console.log(`Docs extracted: ${proximal.length}`);
+
+  console.time('convertToDuration');
+  proximal = convertToDuration(proximal);
+  console.timeEnd('convertToDuration');
+
+  console.log(`Periph dur: ${JSON.stringify(proximal)}`);
+  // proximal = proximal.filter(item => {item.duration >= 15});
+  // window_contacts = rssiCalibration(window_contacts);
+
+  return { proximal };
 }
 
 // notify close contacts
 async function notifyCloseContacts(node_id, ref_date, result_date) {
-  const contacts = await getCloseContact(node_id, ref_date, result_date);
+  const contacts = await getCloseContacts(node_id, ref_date, result_date);
   // insert notification function here
   console.log('NOTIFIED CCs');
-  console.log(contacts.direct);
+  // console.log(contacts.direct);
   console.log(contacts.proximal);
 }
 
-// notifyCloseContacts(node_id, ref_date, result_date);
+notifyCloseContacts(node_id, ref_date, result_date);
--- src/services/node-contact.service.js
+++ src/services/node-contact.service.js
@@ -1,13 +1,19 @@
 /* eslint-disable camelcase */
-// require('../configs/database.config').config();
+require('../configs/database.config').config();
 const NodeContacts = require('~models/node-contact.model');
 
 // TODO: FIX TIMEZONES!
 
-async function getContactsInWindow(node_id, reference_date, result_date) {
+async function getContactsInRange(
+  node_id,
+  reference_date,
+  result_date,
+  dist_range = [-50, -30],
+) {
   /**
-   * Extract contacts made 2 days before onset of illness/testing date, to
-   * when testing results came out.
+   * Within contacts made 2 days before illness/testing date, to
+   * when testing results came out, extract those contacted within dist.
+   * NOTE: Assumes input has already been calibrated
    */
 
   // change reference to start of the day: x-2
@@ -17,18 +23,74 @@ async function getContactsInWindow(node_id, reference_date, result_date) {
   // change result/end date to the end of test result day
   result_date.setHours(24, 0, 0);
 
-  // filter by node_id, and (x-2 : x+a)
+  // filter by node_id, (x-2 : x+a), and rssi (in meters) < dist.
   return NodeContacts
-    .find({ source_node_id: node_id })
-    .where('timestamp').gt(reference_date).lt(result_date)
+    .find({
+      node_pairs: { $eq: node_id },
+      timestamp: { $gte: reference_date, $lte: result_date },
+      rssi: { $gt: dist_range[0], $lte: dist_range[1] },
+    })
+    .select(['node_pairs', 'timestamp'])
+    // .where('timestamp').gt(reference_date).lt(result_date)
+    // .where('rssi')
     .exec();
-
-  // NOTE: May use select() here to minimize output size
 }
 
-function convertToDuration(res) {
-  console.log('Group timestamps of a nodepair into duration');
-  return res;
+function convertToDuration(docs, node_id) {
+  /**
+   * Group timestamps of a contact with a periph into a blob of duration.
+   */
+  const docs_periph = {}; // {periph id : [timestamp...]}
+  const max_gap = 30000; // max gap in a continuous duration, in millisec
+  let periph_id;
+
+  // TODO: determine optimal value for max_gap based on scanning and upload
+  // intervals
+
+  // group timestamps of each peripheral (node pair)
+  docs.forEach((doc) => { // O(n)
+    periph_id = doc.node_pairs[node_id === doc.node_pairs[0] ? 1 : 0];
+    if (periph_id in docs_periph) {
+      docs_periph[periph_id].push(doc.timestamp.getTime());
+    } else {
+      docs_periph[periph_id] = [doc.timestamp.getTime()];
+    }
+  });
+
+  for (const id in docs_periph) {
+    // sort by time
+    docs_periph[id].sort(); // probably O(nlogn)
+
+    // if adjacent timestamp is later than X seconds, consider as diff blob
+    // NOTE: Let's make X=7
+    let time_init = docs_periph[id][0]; // initial time of contact
+    let time_last = time_init; // time of last contact
+    const durations = []; // for blobs of contact duration with a periph
+    const num_periphs = docs_periph[id].length;
+    console.log('--------------------');
+    for (let i = 1; i < num_periphs; i++) { // O(n)
+      time_last = docs_periph[id][i - 1];
+      const time_gap = docs_periph[id][i] - time_last;
+      if (time_gap > max_gap) {
+        // means i belongs to another duration, so take this as a dur
+        console.log(`dur: ${new Date(time_init).toLocaleTimeString()} -- ${new Date(time_last).toLocaleTimeString()}`);
+        console.log(`gap: ${time_gap / 1000}s`);
+        durations.push((time_last - time_init) / 1000 / 60); // in mins
+        time_init = docs_periph[id][i];
+        time_last = time_init;
+      } else if (i === num_periphs - 1) {
+        // reached the end and i is still part of the current blob
+        durations.push((docs_periph[id][i] - time_init) / 1000 / 60); // in mins
+        console.log(`dur: ${new Date(time_init).toLocaleTimeString()} -- ${new Date(time_last).toLocaleTimeString()}`);
+      }
+    }
+    console.log('--------------------');
+    // sum all contact durations
+    // NOTE: This assumes that duration is additive.
+    console.log(`Duration array: ${durations}`);
+    docs_periph[id] = durations.reduce((a, b) => a + b, 0);
+  }
+  return docs_periph;
 }
 
 function rssiCalibration(res) {
@@ -36,14 +98,8 @@ function rssiCalibration(res) {
   return res;
 }
 
-function filter(res, query) {
-  console.log(`filter by ${query}`);
-  return res;
-}
-
 module.exports = {
-  getContactsInWindow,
+  getContactsInRange,
   convertToDuration,
   rssiCalibration,
-  filter,
 };
