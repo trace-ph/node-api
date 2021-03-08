8793f8d016925d3ac2a264e76f1b04ffdfc1d68f
joshfallesgon <joshfallesgon@gmail.com>
2021-03-08 16:13:51 +0800

    Included garbage collector

--- src/services/garbColl.js
+++ src/services/garbColl.js
@@ -1,5 +1,5 @@
-//Import configs to be able to connect to DATABASE:
-require('../configs/database.config').config();
+//Import logger 
+const logger = require('~logger');
 
 //Import Schema model (nodecontact db)
 const NodeContacts = require('~models/node-contact.model');
@@ -8,16 +8,32 @@ const NodeContacts = require('~models/node-contact.model');
 const cron = require('node-cron');
 const shell = require('shelljs');
 
-
-
+//Set date to 3 weeks before current date
 var date = new Date();
 var older_date = new Date();
 older_date.setDate(date.getDate() - 20);
 
-cron.schedule("0 0 * * SUN", function(){
+
+function garbcoll() {
+
+	//Cron scheduler function (running every 12AM of Sundays)
+	cron.schedule("0 0 * * SUN", function(){
+
+    	logger.info("Deleting datas...")
+	NodeContacts.count({ timestamp: {$lt : older_date } }, function( err, count){
+
 
 	NodeContacts.remove({ timestamp: {$lt : older_date } }, function(err) {
-  		console.log("Deletion Complete!");
+		if(err) {
+			logger.error(err);
+		}
+  		logger.info(`Datas Successfully deleted: ${count}`);
 
 	});
 });
+})
+
+};
+
+
+module.exports = {garbcoll};
