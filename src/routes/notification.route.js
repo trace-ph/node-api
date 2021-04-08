95b84c0b3f8b20c6d7c500e023040368f4f7fefc
JelloJill <abrafael1@up.edu.ph>
2021-04-08 17:21:58 +0800

    ✨ Notification confirmation

--- src/routes/notification.route.js
+++ src/routes/notification.route.js
@@ -11,6 +11,7 @@ const connections = {};
 router.post('/', (req, res) => {
 	const { node_id } = req.body;
 
+	// console.log(`${node_id} connected`);
 	connections[node_id] = res;
 
 	// If connection is already disconnected, remove in connections list
@@ -23,11 +24,11 @@ router.post('/', (req, res) => {
 });
 
 
-const delay = 1000 * 5;			// 1 second * multiplier
+const delayNotif = 1000 * 60;				// 1 second * multiplier
 
 // Checks if there's notification to be sent every 1 minute
 setTimeout(function check() {
-	// console.log(connections);
+	logger.info('Checking NotifiedContacts...');
 	for(const [node_id, res] of Object.entries(connections)) {
 		// console.log(node_id);
 		NotifiedContacts.findOne({ node_id: node_id }, (err, doc) => {
@@ -40,25 +41,39 @@ setTimeout(function check() {
 
 				// Delete in connections list
 				delete connections[node_id];
-
-				// Update doc
-				doc.notif = true;
-				doc.save();
-
 				logger.info(`${node_id} is notified`);
 			}
 		});
 	}
 
-	setTimeout(check, delay);
-}, delay);
+	setTimeout(check, delayNotif);
+}, delayNotif);
+
+
+// Receives confirmation of notification
+router.post('/confirm', (req, res) => {
+	const { node_id } = req.body;
+	NotifiedContacts.findOne({ node_id: node_id }, (err, doc) => {
+		// Update doc
+		doc.notif = true;
+		doc.save();
+
+		logger.info(`CONFIRMED: ${node_id} is notified`);
+		res.status(200).send("Confirmation received");
+	});
+});
 
 
 module.exports = router;
 
 /*
 Code Reference:
+> Express req-res
 	https://github.com/satansdeer/req-res-streams/blob/master/index.js
+> JS Objects
 	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 	https://stackoverflow.com/questions/3455405/how-do-i-remove-a-key-from-a-javascript-object
+> Client disconnect
+	https://stackoverflow.com/questions/6572572/node-js-http-server-detect-when-clients-disconnect
+	https://stackoverflow.com/questions/7348736/how-to-check-if-connection-was-aborted-in-node-js-server
 */
