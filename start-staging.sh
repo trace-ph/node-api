d9edaa79063d0e8ad312ca642794e036c5943abe
Patrick Buitre <patrickbuitre@Patricks-MacBook-Pro.local>
2021-05-25 12:50:49 +0800

    Added staging script

---
+++ start-staging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source .env
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo 'Error: NPM is not installed. Install NodeJS manually or use nvm before running' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed. Install docker and/or docker-compose manually before running' >&2
+  exit 1
+fi
+
+
+echo "-------BUILDING PROD WEB API----------"
+docker-compose -f ./docker-compose.staging.yml build --no-cache
+
+echo "-------STARTING API SERVICE----------"
+docker-compose -f ./docker-compose.staging.yml up -d
